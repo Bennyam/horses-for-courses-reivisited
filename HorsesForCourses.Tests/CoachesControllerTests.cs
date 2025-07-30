67763065c521479fe91f1328238468625ce60c62
Bennyam <38783044+Bennyam@users.noreply.github.com>
2025-07-30 19:08:41 +0200

    Improved test percentage, total prject 96% tested

--- HorsesForCourses.Tests/CoachesControllerTests.cs
+++ HorsesForCourses.Tests/CoachesControllerTests.cs
@@ -8,68 +8,101 @@ namespace HorsesForCourses.Tests;
 
 public class CoachesControllerTests
 {
-    private readonly CoachesController _controller;
+  private readonly CoachesController _controller;
 
-    public CoachesControllerTests()
+  public CoachesControllerTests()
+  {
+    var repo = new InMemoryCoachRepository();
+    var service = new CoachService(repo);
+    _controller = new CoachesController(service);
+  }
+
+  [Fact]
+  public void CreateCoach_ReturnsOk_WithValidId()
+  {
+    var dto = new CreateCoachDto
     {
-        var repo = new InMemoryCoachRepository();
-        var service = new CoachService(repo);
-        _controller = new CoachesController(service);
-    }
+      Name = "Ben",
+      Email = "ben@coach.be"
+    };
 
-    [Fact]
-    public void CreateCoach_ReturnsOk_WithValidId()
-    {
-        var dto = new CreateCoachDto
-        {
-            Name = "Ben",
-            Email = "ben@coach.be"
-        };
+    var result = _controller.CreateCoach(dto) as OkObjectResult;
 
-        var result = _controller.CreateCoach(dto) as OkObjectResult;
+    Assert.NotNull(result);
+    Assert.IsType<Guid>(result!.Value);
+  }
 
-        Assert.NotNull(result);
-        Assert.IsType<Guid>(result!.Value);
-    }
+  [Fact]
+  public void UpdateSkills_ReturnsNoContent_WhenValid()
+  {
+    var createDto = new CreateCoachDto { Name = "Anna", Email = "anna@coach.be" };
+    var createResult = _controller.CreateCoach(createDto) as OkObjectResult;
+    var id = (Guid)createResult!.Value!;
 
-    [Fact]
-    public void UpdateSkills_ReturnsNoContent_WhenValid()
-    {
-        var createDto = new CreateCoachDto { Name = "Anna", Email = "anna@coach.be" };
-        var createResult = _controller.CreateCoach(createDto) as OkObjectResult;
-        var id = (Guid)createResult!.Value!;
+    var skillsDto = new UpdateCoachSkillsDto { Skills = new List<string> { "C#", "Agile" } };
 
-        var skillsDto = new UpdateCoachSkillsDto { Skills = new List<string> { "C#", "Agile" } };
+    var result = _controller.UpdateSkills(id, skillsDto);
 
-        var result = _controller.UpdateSkills(id, skillsDto);
+    Assert.IsType<NoContentResult>(result);
+  }
 
-        Assert.IsType<NoContentResult>(result);
-    }
+  [Fact]
+  public void GetById_ReturnsCoachDto_WhenFound()
+  {
+    var createDto = new CreateCoachDto { Name = "Tom", Email = "tom@coach.be" };
+    var createResult = _controller.CreateCoach(createDto) as OkObjectResult;
+    var id = (Guid)createResult!.Value!;
 
-    [Fact]
-    public void GetById_ReturnsCoachDto_WhenFound()
-    {
-        var createDto = new CreateCoachDto { Name = "Tom", Email = "tom@coach.be" };
-        var createResult = _controller.CreateCoach(createDto) as OkObjectResult;
-        var id = (Guid)createResult!.Value!;
+    var result = _controller.GetById(id) as OkObjectResult;
 
-        var result = _controller.GetById(id) as OkObjectResult;
+    Assert.NotNull(result);
+    Assert.Equal(id, ((CoachDto)result!.Value!).Id);
+  }
 
-        Assert.NotNull(result);
-        Assert.Equal(id, ((CoachDto)result!.Value!).Id);
-    }
+  [Fact]
+  public void GetAllCoaches_ReturnsListOfCoaches()
+  {
+    _controller.CreateCoach(new CreateCoachDto { Name = "A", Email = "a@x.be" });
+    _controller.CreateCoach(new CreateCoachDto { Name = "B", Email = "b@x.be" });
 
-    [Fact]
-    public void GetAllCoaches_ReturnsListOfCoaches()
-    {
-        _controller.CreateCoach(new CreateCoachDto { Name = "A", Email = "a@x.be" });
-        _controller.CreateCoach(new CreateCoachDto { Name = "B", Email = "b@x.be" });
+    var result = _controller.GetAllCoaches() as OkObjectResult;
 
-        var result = _controller.GetAllCoaches() as OkObjectResult;
+    Assert.NotNull(result);
+    var list = result!.Value as IEnumerable<CoachListItemDto>;
+    Assert.NotNull(list);
+    Assert.True(list!.Count() >= 2);
+  }
+    
+  [Fact]
+  public void CreateCoach_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.CreateCoach(null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
 
-        Assert.NotNull(result);
-        var list = result!.Value as IEnumerable<CoachListItemDto>;
-        Assert.NotNull(list);
-        Assert.True(list!.Count() >= 2);
-    }
+  [Fact]
+  public void UpdateSkills_ReturnsProblem_WhenCoachNotFound()
+  {
+      var dto = new UpdateCoachSkillsDto { Skills = new List<string> { "X" } };
+      var result = _controller.UpdateSkills(Guid.NewGuid(), dto) as ObjectResult;
+
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void UpdateSkills_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.UpdateSkills(Guid.NewGuid(), null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void GetById_ReturnsProblem_WhenCoachNotFound()
+  {
+      var result = _controller.GetById(Guid.NewGuid());
+      Assert.IsType<ObjectResult>(result);
+  }
 }
--- HorsesForCourses.Tests/CoursesControllerTests.cs
+++ HorsesForCourses.Tests/CoursesControllerTests.cs
@@ -9,70 +9,159 @@ namespace HorsesForCourses.Tests;
 
 public class CoursesControllerTests
 {
-    private readonly CoursesController _controller;
+  private readonly CoursesController _controller;
 
-    public CoursesControllerTests()
+  public CoursesControllerTests()
+  {
+    var courseRepo = new InMemoryCourseRepository();
+    var coachRepo = new InMemoryCoachRepository();
+
+    var service = new CourseService(courseRepo, coachRepo);
+    _controller = new CoursesController(service);
+  }
+
+  [Fact]
+  public void CreateCourse_ReturnsOk_WithValidId()
+  {
+    var dto = new CreateCourseDto
     {
-        var courseRepo = new InMemoryCourseRepository();
-        var coachRepo = new InMemoryCoachRepository();
+      Name = "Test Course",
+      StartDate = new DateOnly(2025, 10, 1),
+      EndDate = new DateOnly(2025, 10, 10)
+    };
 
-        var service = new CourseService(courseRepo, coachRepo);
-        _controller = new CoursesController(service);
-    }
+    var result = _controller.CreateCourse(dto) as OkObjectResult;
 
-    [Fact]
-    public void CreateCourse_ReturnsOk_WithValidId()
+    Assert.NotNull(result);
+    Assert.IsType<Guid>(result!.Value);
+  }
+
+  [Fact]
+  public void Confirm_ReturnsNoContent_WhenCourseExists()
+  {
+    var dto = new CreateCourseDto
     {
-        var dto = new CreateCourseDto
-        {
-            Name = "Test Course",
-            StartDate = new DateOnly(2025, 10, 1),
-            EndDate = new DateOnly(2025, 10, 10)
-        };
+      Name = "Course to Confirm",
+      StartDate = new DateOnly(2025, 10, 1),
+      EndDate = new DateOnly(2025, 10, 10)
+    };
 
-        var result = _controller.CreateCourse(dto) as OkObjectResult;
+    var createResult = _controller.CreateCourse(dto) as OkObjectResult;
+    var courseId = (Guid)createResult!.Value!;
 
-        Assert.NotNull(result);
-        Assert.IsType<Guid>(result!.Value);
-    }
-
-    [Fact]
-    public void Confirm_ReturnsNoContent_WhenCourseExists()
+    _controller.UpdateSkills(courseId, new UpdateCourseSkillsDto
     {
-        var dto = new CreateCourseDto
-        {
-            Name = "Course to Confirm",
-            StartDate = new DateOnly(2025, 10, 1),
-            EndDate = new DateOnly(2025, 10, 10)
-        };
+      Skills = new List<string> { "C#" }
+    });
 
-        var createResult = _controller.CreateCourse(dto) as OkObjectResult;
-        var courseId = (Guid)createResult!.Value!;
-
-        _controller.UpdateSkills(courseId, new UpdateCourseSkillsDto
-        {
-            Skills = new List<string> { "C#" }
-        });
-
-        _controller.UpdateTimeslots(courseId, new UpdateCourseTimeslotsDto
-        {
-            Timeslots = new List<TimeslotDto>
+    _controller.UpdateTimeslots(courseId, new UpdateCourseTimeslotsDto
+    {
+      Timeslots = new List<TimeslotDto>
             {
                 new() { Day = "Monday", Start = 9, End = 11 }
             }
-        });
+    });
 
-        var result = _controller.Confirm(courseId);
+    var result = _controller.Confirm(courseId);
 
-        Assert.IsType<NoContentResult>(result);
-    }
+    Assert.IsType<NoContentResult>(result);
+  }
 
-    [Fact]
-    public void GetAllCourses_ReturnsOk_WithList()
-    {
-        var result = _controller.GetAllCourses() as OkObjectResult;
+  [Fact]
+  public void GetAllCourses_ReturnsOk_WithList()
+  {
+    var result = _controller.GetAllCourses() as OkObjectResult;
 
-        Assert.NotNull(result);
-        Assert.IsAssignableFrom<IEnumerable<CourseListItemDto>>(result!.Value);
-    }
+    Assert.NotNull(result);
+    Assert.IsAssignableFrom<IEnumerable<CourseListItemDto>>(result!.Value);
+  }
+    
+  [Fact]
+  public void CreateCourse_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.CreateCourse(null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void Confirm_ReturnsProblem_WhenCourseNotFound()
+  {
+      var result = _controller.Confirm(Guid.NewGuid()) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void UpdateSkills_ReturnsProblem_WhenCourseNotFound()
+  {
+      var dto = new UpdateCourseSkillsDto { Skills = new List<string> { "X" } };
+      var result = _controller.UpdateSkills(Guid.NewGuid(), dto) as ObjectResult;
+
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void UpdateSkills_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.UpdateSkills(Guid.NewGuid(), null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void UpdateTimeslots_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.UpdateTimeslots(Guid.NewGuid(), null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void UpdateTimeslots_ReturnsProblem_WhenInvalidId()
+  {
+      var dto = new UpdateCourseTimeslotsDto
+      {
+          Timeslots = new List<TimeslotDto> {
+              new() { Day = "Monday", Start = 9, End = 11 }
+          }
+      };
+
+      var result = _controller.UpdateTimeslots(Guid.NewGuid(), dto) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void AssignCoach_ReturnsProblem_WhenDtoIsNull()
+  {
+      var result = _controller.AssignCoach(Guid.NewGuid(), null!) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void AssignCoach_ReturnsProblem_WhenCoachNotFound()
+  {
+      var courseId = _controller.CreateCourse(new CreateCourseDto
+      {
+          Name = "Test",
+          StartDate = new DateOnly(2025, 10, 1),
+          EndDate = new DateOnly(2025, 10, 10)
+      }) as OkObjectResult;
+
+      var dto = new AssignCoachDto { CoachId = Guid.NewGuid() };
+
+      var result = _controller.AssignCoach((Guid)courseId!.Value!, dto) as ObjectResult;
+      Assert.NotNull(result);
+      Assert.Equal(500, result!.StatusCode);
+  }
+
+  [Fact]
+  public void GetById_ReturnsProblem_WhenCourseNotFound()
+  {
+      var result = _controller.GetById(Guid.NewGuid());
+      Assert.IsType<ObjectResult>(result);
+  }
 }
